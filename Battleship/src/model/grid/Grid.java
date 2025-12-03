d109811e8b72f56eb304ac9a9c847e79516a062b
Hugo Leger <legerh@unistra.fr>
2025-12-03 14:56:12 +0100

    SRC : Ajout des attributs m_size,m_grid,m_gridObservers de grid. Ajout des méthodes hitTile, placeObject mais pas implémenter encore, addObserver, removeObserver, notifyTileHit.

--- Battleship/src/model/grid/Grid.java
+++ Battleship/src/model/grid/Grid.java
@@ -1,4 +1,36 @@
 package model.grid;
 
+import model.placableObject.Trap.PlaceableObject;
+
+import java.util.ArrayList;
+
 public class Grid {
+    private int  m_size;
+    private ArrayList<Tile> m_grid;
+    private ArrayList<GridObserver> m_gridObservers;
+    public Grid(int size){
+        this.m_size = size;
+        this.m_grid = new ArrayList<Tile>();
+        this.m_gridObservers = new ArrayList<GridObserver>();
+    }
+    public void hitTile(int pos){
+        m_grid.get(pos).hit();
+        notifyTileHit(pos);
+    }
+    public void placeObject(PlaceableObject object){
+        //m_grid.get()
+    }
+    public void addObserver(GridObserver obs){
+        m_gridObservers.add(obs);
+    }
+    public void removeObserver(GridObserver obs){
+        m_gridObservers.remove(obs);
+    }
+    public void notifyTileHit(int pos){
+        m_gridObservers.forEach((obs)->{
+            obs.updateTileHit(pos);
+                }
+        );
+    }
+
 }
