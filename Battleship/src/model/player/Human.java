4fc1b4dc44695ba8a7c8d23a28443c2b46d441fd
Thomas Barseghian <tbarseghian@unistra.fr>
2025-12-08 14:46:04 +0100

    src : rename Human

--- Battleship/src/model/player/Human.java
+++ Battleship/src/model/player/Human.java
@@ -7,24 +7,19 @@ import java.util.ArrayList;
 
 import static model.placeableObject.ship.Direction.VERTICAL;
 
-public class Humain extends Player{
-    public Humain(String name, Grid grid, ArrayList<Ship> ships)
+public class Human extends Player{
+    public Human(String name, Grid grid, ArrayList<Ship> ships)
     {
         super(name, grid, ships);
     }
 
     @Override
-    public void placeShipFix()
-    {
+    public void placeShipFix() {
         //Works if each Player has 5 ships
-        for (int i=0; i<5; i++)
-        {
-            Ship currentShip = m_ships.get(i);
-            //currentShip.setDirection(VERTICAL);
-            currentShip.setPosition(0);
-        }
+        Ship currentShip = m_ships.get(0);
+        currentShip.setDirection(VERTICAL);
+        currentShip.setPosition(0);
     }
-
     @Override
     public void placeTrapFix()
     {
--- Battleship/src/model/player/PlayerObserver.java
+++ Battleship/src/model/player/PlayerObserver.java
@@ -4,22 +4,10 @@ import model.placeableObject.Trap.Trap;
 import model.placeableObject.Weapon.Weapon;
 import model.placeableObject.ship.Ship;
 
-public class PlayerObserver {
+public interface PlayerObserver {
 
-    public void updateShips(Ship[] ships)
-    {
-
-    }
-    public void updateTraps(Trap[] traps)
-    {
-
-    }
-    public void updateWeapons(Weapon[] weapons)
-    {
-
-    }
-    public void updateAttack(int pos, Weapon weapon)
-    {
-
-    }
+    public void updateShips(Ship[] ships);
+    public void updateTraps(Trap[] traps);
+    public void updateWeapons(Weapon[] weapons);
+    public void updateAttack(int pos, Weapon weapon);
 }
